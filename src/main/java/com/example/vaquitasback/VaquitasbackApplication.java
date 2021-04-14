7065f3a31ba583114bcf9f1bb98cd4dced0cf142
flakoash <flash.ar79@gmail.com>
2021-04-14 11:12:32 -0400

    initial commit

---
+++ src/main/java/com/example/vaquitasback/VaquitasbackApplication.java
@@ -0,0 +1,14 @@
+package com.example.vaquitasback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class VaquitasbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaquitasbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vaquitasback/VaquitasbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaquitasback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaquitasbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
