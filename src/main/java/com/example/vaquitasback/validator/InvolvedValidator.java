ae436db941548d9186eb5780b017546789516f5f
flakoash <flash.ar79@gmail.com>
2021-04-20 12:00:51 -0400

    fix: fixed bug BigDecimal(8)!==BigDecimal(8.0)

--- src/main/java/com/example/vaquitasback/validator/InvolvedValidator.java
+++ src/main/java/com/example/vaquitasback/validator/InvolvedValidator.java
@@ -2,6 +2,8 @@ package com.example.vaquitasback.validator;
 
 import com.example.vaquitasback.entity.Involved;
 import com.example.vaquitasback.entity.Transaction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,12 +13,15 @@ import java.math.BigDecimal;
 
 
 public class InvolvedValidator implements ConstraintValidator<InvolvedSumUp, Transaction> {
+    Logger logger = LoggerFactory.getLogger(InvolvedValidator.class);
+
     @Override
     public boolean isValid(Transaction transaction, ConstraintValidatorContext constraintValidatorContext) {
-        BigDecimal sum = BigDecimal.valueOf(0);
+        BigDecimal sum = new BigDecimal("0.0");
         for(Involved involved : transaction.getInvolved()){
             sum = sum.add(involved.getAmount());
         }
-        return sum.equals(transaction.getAmount());
+        logger.warn("sum: "+sum + " val: "+transaction.getAmount());
+        return sum.equals(transaction.getAmount().add(new BigDecimal("0.0")));
     }
 }
