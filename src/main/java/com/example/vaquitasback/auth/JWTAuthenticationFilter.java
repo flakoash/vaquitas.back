c02c9d2557137f0e88ccd2476bee6e6a6e72c382
flakoash <flash.ar79@gmail.com>
2021-05-03 11:06:54 -0400

    feat: changed login response to return json object

--- src/main/java/com/example/vaquitasback/auth/JWTAuthenticationFilter.java
+++ src/main/java/com/example/vaquitasback/auth/JWTAuthenticationFilter.java
@@ -5,6 +5,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.example.vaquitasback.entity.User;
 import com.example.vaquitasback.web.UserController;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +13,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.boot.configurationprocessor.json.JSONObject;
 
 import javax.servlet.FilterChain;
 import javax.servlet.http.HttpServletRequest;
@@ -19,6 +21,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+
 
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     Logger logger = LoggerFactory.getLogger(JWTAuthenticationFilter.class);
@@ -50,6 +53,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         }
     }
 
+    @SneakyThrows
     @Override
     protected void successfulAuthentication(HttpServletRequest req,
                                             HttpServletResponse res,
@@ -60,9 +64,17 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.EXPIRATION_TIME))
                 .sign(Algorithm.HMAC512(SecurityConstants.SECRET.getBytes()));
 
-        String body = ((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername() + " " + token;
+//        String body = ((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername() + " " + token;
+//        String body = token;
+        JSONObject responseObject =  new JSONObject();
+        String uname = ((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername();
+        responseObject.put("username", uname);
+        responseObject.put("token", token);
 
-        res.getWriter().write(body);
+
+        res.setContentType("application/json");
+        res.setCharacterEncoding("UTF-8");
+        res.getWriter().write(responseObject.toString());
         res.getWriter().flush();
     }
 }
