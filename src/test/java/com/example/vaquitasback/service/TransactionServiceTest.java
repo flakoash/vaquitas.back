de9b3aac1aaee158a1180609f4dafa48d0cbb3ad
flakoash <flash.ar79@gmail.com>
2021-05-07 18:14:21 -0400

    add: test that just members of group can query transactions

--- src/test/java/com/example/vaquitasback/service/TransactionServiceTest.java
+++ src/test/java/com/example/vaquitasback/service/TransactionServiceTest.java
@@ -6,6 +6,8 @@ import com.example.vaquitasback.entity.Involved;
 import com.example.vaquitasback.entity.Transaction;
 import com.example.vaquitasback.entity.User;
 import com.example.vaquitasback.repository.TransactionRepository;
+import com.example.vaquitasback.repository.UserRepository;
+import com.example.vaquitasback.web.ForbiddenException;
 import org.hibernate.validator.HibernateValidator;
 import org.junit.Before;
 import org.junit.Test;
@@ -37,8 +39,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.StreamSupport;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
@@ -51,13 +52,20 @@ public class TransactionServiceTest {
     @Autowired
     ApplicationUserDetailsService userDetailsService;
 
+    @Mock
+    GroupService groupService;
+
     TransactionService transactionService;
+
     @Mock
     TransactionRepository transactionRepository;
 
+    @Mock
+    UserRepository userRepository;
+
     @Before
     public void init() {
-        transactionService = new TransactionService(transactionRepository);
+        transactionService = new TransactionService(transactionRepository, groupService, userRepository);
     }
 
     @Test
@@ -74,12 +82,59 @@ public class TransactionServiceTest {
         tr.setInvolved(invList);
         listTr.add(tr);
 
+        // Auth user
+        UserDetails userDetails = userDetailsService.loadUserByUsername ("testUser"); // user id 1
+        Authentication authToken = new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        // userRepo
+        when(userRepository.findByUsername("testUser") ).thenReturn(owner);
+
+        // userRepo
+        when(groupService.getMembers(1) ).thenReturn(new ArrayList<User>(Arrays.asList(owner)));
+
+        // transactionRepo
         when(transactionRepository.getTransactionsByGroup_idOrderByCreatedAtDesc(1) ).thenReturn(listTr);
 
         //test
         Iterable<Transaction> transactionList = transactionService.getTransactions(1);
 
         assertEquals(1, StreamSupport.stream(transactionList.spliterator(), false).count());
+    }
+
+    @Test
+    public void otherAuthUserGetTransactionsTest() {
+        BigDecimal amount = new BigDecimal(100);
+
+        User authUser = new User(1, "testUser", "test user", "1234567", "None");
+        User owner = new User(2, "testUser2", "test user", "1234567", "None");
+        Group group = new Group(1, "testGroup", "None", new ArrayList<User>(Arrays.asList(owner)));
+
+
+        List<Involved> invList =  new ArrayList<>(Arrays.asList(new Involved(1, owner, amount)));
+        List<Transaction> listTr = new ArrayList<>();
+        Transaction tr = new Transaction(1, "Test transaction", "test description", amount, owner, group);
+        tr.setInvolved(invList);
+        listTr.add(tr);
+
+        // Auth user
+        UserDetails userDetails = userDetailsService.loadUserByUsername ("testUser"); // user id 1
+        Authentication authToken = new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        // userRepo
+        when(userRepository.findByUsername("testUser") ).thenReturn(authUser);
+
+        // userRepo
+        when(groupService.getMembers(1) ).thenReturn(new ArrayList<User>(Arrays.asList(owner)));
+
+        // transactionRepo
+        when(transactionRepository.getTransactionsByGroup_idOrderByCreatedAtDesc(1) ).thenReturn(listTr);
+
+        //test
+        Exception exception = assertThrows(ForbiddenException.class, () -> {
+            transactionService.getTransactions(1);
+        });
     }
 
     @Test
@@ -175,5 +230,31 @@ public class TransactionServiceTest {
         assertEquals(1, violations.size());
         ConstraintViolation<?> violation = violations.iterator().next();
         assertEquals("Only members of the group can add transactions", violation.getMessage() );
+    }
+
+    @Test
+    public void OwnerDiffThanAuthUserAddTransactionsTest() {
+        BigDecimal amount = new BigDecimal(100);
+
+        User owner = new User(2, "testUser", "test user", "1234567", "None");
+        Group group = new Group(1, "testGroup", "None", new ArrayList<User>(Arrays.asList(owner)));
+
+        // transaction
+        List<Involved> invList =  new ArrayList<>(Arrays.asList(new Involved(1, owner, amount)));
+        Transaction tr = new Transaction(1, "Test transaction", "test description", amount, owner, group);
+        tr.setInvolved(invList);
+
+        // Auth user
+        UserDetails userDetails = userDetailsService.loadUserByUsername ("testUser");
+        Authentication authToken = new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(), userDetails.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+
+        // test
+        Set<ConstraintViolation<Transaction>> violations = validator.validate(tr);
+
+        assertEquals(1, violations.size());
+//        ConstraintViolation<?> violation = violations.iterator().next();
+//        assertEquals("Only members of the group can add transactions", violation.getMessage() );
     }
 }
